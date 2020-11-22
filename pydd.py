1f186684220d3b7740c189eb50608c3200cf6d66
Tal <tal@server.fake>
2020-11-22 10:55:33 -0700

    Added ability to tell the type of device, and the size of it
    Also checks that we have permissions to read from input_file, and write
    to output_file

--- pydd.py
+++ pydd.py
@@ -4,29 +4,75 @@
 # Imports #
 ###########
 
+import re
+import os
+import sys
 import argparse
 import textwrap
-import re
+from pathlib import Path
 
 ###############
 # Global Vars #
 ###############
 
+# Bash color escape codes
+RED='\033[00;31m'
+NORMAL='\033[0m'
+
 #############
 # Functions #
 #############
 
-# Validates that the data looks like a proper size value
-# Returns the size in bytes
-# Accepts suffixes:
-#   K - Kibibyte
-#   M - Mebibyte
-#   G - Gibibyte
-#   T - Tebibyte
-#   P - Pebibyte
-#   E - Exbibyte
-#   Z - Zebibyte
+def blockdev_size(path):
+    """
+        Return block device size in bytes
+        Source: https://stackoverflow.com/a/57137214/6423456
+    """
+
+    with open(path, 'rb') as f:
+        return f.seek(0, 2) or f.tell()
+
+def eprint(s, **kwargs):
+    """
+        Print to stderr
+    """
+    print(f"{RED}ERROR{NORMAL}: {s}", file=sys.stderr, **kwargs)
+
+def permissions_check(input_file, output_file):
+    """
+        Make sure we have permissions to read from input_file, and write
+        to output_file
+    """
+
+    # Make sure the input file exists
+    if not input_file.exists():
+        eprint(f"{input_file} does not exist")
+        sys.exit(1)
+
+    # Make sure we have the permissions to read from input_file
+    if not os.access(input_file, os.R_OK):
+        eprint(f"No permission to read {input_file}")
+        sys.exit(1)
+
+    # Make sure we have the permissions to write to output_file
+    if not os.access(output_file, os.W_OK):
+        eprint(f"No permission to write to {output_file}")
+        sys.exit(1)
+
 def size(s):
+    """
+        Validates that the data looks like a proper size value
+        Returns the size in bytes
+        Accepts suffixes:
+          K - Kibibyte
+          M - Mebibyte
+          G - Gibibyte
+          T - Tebibyte
+          P - Pebibyte
+          E - Exbibyte
+          Z - Zebibyte
+    """
+
     pattern = '^[0-9]+[kmgtpez]?$'
 
     # Make sure it looks right
@@ -118,4 +164,39 @@ if __name__ == "__main__":
     # Parse sys.argv using the rules defined above
     args = parser.parse_args()
 
+    # Turn input file and output file intp pathlib Path objects
+    input_file = Path(args.input_file)
+    output_file = Path(args.output_file)
+
+    # Ensure permissions for reading from source, and writing to dest
+    permissions_check(input_file, output_file)
+
+    # Figure out amount of data we want to read
+    if args.count:
+        # We were told explicitly how much data to read
+        bytes_to_read = args.bs * args.count
+    else:
+        # Try to figure it out
+        if args.input_file == "/dev/stdin":
+            # Input is a pipe - we don't know unless we were told
+            # explicitly using the --ts arg. If the user didn't specify
+            # --ts, args.ts will be None, so bytes_to_read will be set
+            # to None
+            bytes_to_read = args.ts
+        else:
+            # Check the type of file it is
+            if input_file.is_block_device():
+                # This is a block device
+                print(f"{args.input_file} is a block device")
+                bytes_to_read = blockdev_size(args.input_file)
+            elif input_file.is_file():
+                # This is a regular file
+                print(f"{args.input_file} is a regular file")
+                bytes_to_read = os.stat(args.input_file).st_size
+            else:
+                # This is an unsupported file type
+                eprint(f"{args.input_file} is an unsupported filetype")
+                sys.exit(1)
+
     print(args)
+    print(f"Bytes to Read: {bytes_to_read}")
