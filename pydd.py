38a49eeaf8188dcf436672057b65038900ea8a63
Tal <tal@server.fake>
2020-11-22 08:25:45 -0700

    Added validation for --bs and --ts
    Also converts to proper value in bytes as int

--- pydd.py
+++ pydd.py
@@ -6,10 +6,50 @@
 
 import argparse
 import textwrap
+import re
 
 ###############
 # Global Vars #
 ###############
+
+#############
+# Functions #
+#############
+
+# Validates that the data looks like a proper size value
+# Returns the size in bytes
+# Accepts suffixes:
+#   K - Kibibyte
+#   M - Mebibyte
+#   G - Gibibyte
+#   T - Tebibyte
+#   P - Pebibyte
+#   E - Exbibyte
+#   Z - Zebibyte
+def size(s):
+    pattern = '^[0-9]+[kmgtpez]?$'
+
+    # Make sure it looks right
+    if not re.search(pattern, s, re.IGNORECASE):
+        raise ValueError()
+
+    # Convert to int value in bytes
+    if s.isdigit():
+        return int(s)
+    elif 'k' in s.lower():
+        return int(s[:-1]) * 1024
+    elif 'm' in s.lower():
+        return int(s[:-1]) * 1024 ** 2
+    elif 'g' in s.lower():
+        return int(s[:-1]) * 1024 ** 3
+    elif 't' in s.lower():
+        return int(s[:-1]) * 1024 ** 4
+    elif 'p' in s.lower():
+        return int(s[:-1]) * 1024 ** 5
+    elif 'e' in s.lower():
+        return int(s[:-1]) * 1024 ** 6
+    elif 'z' in s.lower():
+        return int(s[:-1]) * 1024 ** 7
 
 #############
 # Arguments #
@@ -53,11 +93,13 @@ parser.add_argument(
     "--bs", "--block-size",
     metavar="SIZE",
     help="Specify the blocksize (Default: 4M)",
+    type=size,
     default="4M")
 
 parser.add_argument(
     "--ts", "--total-size",
     metavar="SIZE",
+    type=size,
     help="Total size in bytes (for use with pipes)")
 
 parser.add_argument(
